d4fba3bf80532c0c183f52d4515a4e585dd9342c
Dany Dubé <74633244+danydube1971@users.noreply.github.com>
2023-02-18 08:49:50 -0500

    Update OCR Extract.py

--- OCR Extract.py
+++ OCR Extract.py
@@ -1,4 +1,7 @@
-'''Ce script utilise la fonction input pour demander à l'utilisateur d'entrer le nom d'un image à extraire du texte. Ensuite, il utilise os pour définir le chemin d'accès à l'image dans le dossier courant et charge l'image en mémoire en utilisant PIL. Ensuite, il utilise pytesseract pour extraire le texte de l'image et stocke le résultat dans la variable text. Enfin, le texte est écrit dans un fichier texte nommé resultat_extraction.txt en utilisant with open().
+'''Ce script utilise la fonction input pour demander à l'utilisateur d'entrer le nom d'un image à extraire du texte. 
+Ensuite, il utilise os pour définir le chemin d'accès à l'image dans le dossier courant et charge l'image en mémoire 
+en utilisant PIL. Ensuite, il utilise pytesseract pour extraire le texte de l'image et stocke le résultat dans la variable text. 
+Enfin, le texte est écrit dans un fichier texte nommé resultat_extraction.txt en utilisant with open().
 
 Vous devez installer les paquets :
 apt-get install tesseract-ocr
